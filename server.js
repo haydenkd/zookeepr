2f2b24fff2527cd5b4d370e1606ef7d4d31c0893
Hayden <haydenkd@protonmail.com>
2021-02-20 23:06:01 -0800

    implemented front end

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
+
+app.use(express.static('public'));
 const PORT = process.env.PORT || 3001;
 
 const { animals } = require('./data/animals.json');
@@ -76,6 +78,18 @@ app.get('/api/animals/:id', (req, res) => {
     } else {
         res.send(404);
     }
+})
+
+app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
 })
 
 app.get('/api/animals', (req, res) => {
